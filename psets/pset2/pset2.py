a21f2ebdf3366ee63f7fcb5b9098fdf20f85948d
Gordon-C <c.gordo97@gmail.com>
2022-01-03 18:44:39 -0500

    first part of pset2

---
+++ psets/pset2/pset2.py
@@ -0,0 +1,26 @@
+"""
+1a. Write a for-loop that prints out the integers from 1 to 10 (ascending), inclusive.
+"""
+
+# TODO(student): implement
+
+
+"""
+1b. Write a while-loop that prints out the integers from 10 to 1 (descending), inclusive.
+"""
+
+# TODO(student): implement
+
+
+"""
+1c. Write a loop that prints out all the divisors of 365.
+"""
+
+# TODO(student): implement
+
+
+"""
+1d. Let f(x) = x^2 - 8x + 15. Write a loop that finds what integer within the range [-50,50] yields the smallest f(x) value.
+"""
+
+# TODO(student): implement
