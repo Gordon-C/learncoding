08c054af7f01658c6f7925e7e7ea9b0abd43c3ed
Gordon-C <c.gordo97@gmail.com>
2022-01-03 19:18:34 -0500

    finished pset2

--- psets/pset2/pset2.py
+++ psets/pset2/pset2.py
@@ -24,3 +24,89 @@
 """
 
 # TODO(student): implement
+
+
+"""
+2a. Declare a tuple containing the 7 continents of the world as strings. Print the last element of the tuple.
+"""
+
+# TODO(student): implement
+
+
+"""
+2b. Declare a list containing the numbers 2, 3 and 5. Print the 2nd element of the list.
+"""
+
+# TODO(student): implement
+
+
+"""
+2c. Without using the len() function or the size of the list `xs`, print the last element of `xs`.
+"""
+
+xs = [91.5, 30.8, 101.3]
+# TODO(student): implement
+
+
+"""
+2d. Add the element 15.6 to the end of the list `xs`.
+"""
+
+# TODO(student): implement
+
+
+"""
+3a. Print the average (i.e. arithmetic mean) of the list `xs`.
+"""
+
+# TODO(student): implement
+
+
+"""
+3b. Update the list `xs` by adding 1.0 to each of its elements.
+"""
+
+# TODO(student): implement
+
+
+"""
+3c. Write code that finds and prints the smallest element in list `xs`.
+"""
+
+
+# TODO(student): implement
+
+
+"""
+4a. Declare a set containing the English vowels, and another set
+containing the letters in your name. Without any loops, compute a
+set that contains the vowels in your name. 
+"""
+
+# TODO(student): implement
+
+"""
+4b. The dictionary `distances` maps countries to their distance from the North Pole. 
+Write a loop that identifies which country is furthest from the North Pole. Print the
+name of this country.
+"""
+
+distances = {'Poland': 1024, 'China', 3287, 'Brazil': 6003}
+# TODO(student): implement
+
+
+"""
+4c. A new country, 'Newcountrium', recently came into existence. It is 5 units away from
+the North Pole. Add this data to the dictionary `distances`.
+"""
+
+# TODO(student): implement
+
+
+"""
+4d. Write some code to invert the dictionary `distances`. That is, create a new dictionary
+that maps 1024 -> 'Poland', 3287 -> 'China', etc. Your code should work for any dictionary,
+and not just the specific one contained in `distances`. 
+"""
+
+# TODO(student): implement
